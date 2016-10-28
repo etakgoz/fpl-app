2e6d62dcb6bb36678c01995bdb72a25777c31256
Emin Tolga Akgöz <tolga.akgoz@gmail.com>
2016-10-29 00:18:41 +0200

    initial firebase integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-
 import { routing }      from './app.routing';
+
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
 import { LiveTableComponent } from './live-table/live-table.component';
@@ -22,7 +23,13 @@ import { AppFooterComponent } from './app-footer/app-footer.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    routing
+    routing,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyDCRH6p4-QlwrDDSljm8ODHx_nX9Qc44Uw",
+      authDomain: "fpl-test.firebaseapp.com",
+      databaseURL: "https://fpl-test.firebaseio.com",
+      storageBucket: "fpl-test.appspot.com"
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/live-table/live-table.component.ts
+++ src/app/live-table/live-table.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-live-table',
   templateUrl: './live-table.component.html',
   styleUrls: ['./live-table.component.css']
 })
+
+
 export class LiveTableComponent implements OnInit {
 
-  constructor() { }
+  items: FirebaseListObservable<any[]>;
+
+  constructor(af: AngularFire) {
+    this.items = af.database.list('/league/110296/players');
+  }
 
   ngOnInit() {
   }

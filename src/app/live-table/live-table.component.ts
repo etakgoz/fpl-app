fc7e301bfd4cd653a4538b5b0e46f920291a3b86
Emin Tolga Akgöz <tolga.akgoz@gmail.com>
2017-01-15 01:21:21 +0100

    implemented basic table view

--- src/app/live-table/live-table.component.ts
+++ src/app/live-table/live-table.component.ts
@@ -10,14 +10,60 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 export class LiveTableComponent implements OnInit {
 
-  items: Object[];
+  players: Object[];
+  currentGameweek: Number;
+  playersMap: Object;
+  results: Array<Object>[];
 
   constructor(private _ngZone: NgZone, af: AngularFire) {
-    af.database.list('/league/110296/players').subscribe(items => {
-      this._ngZone.run(() => {
-        this.items = items;
-      })
+    af.database.list('/league/110296/players').subscribe(players => {
+      let map = {};
+      this.players = players;
+
+      players.forEach(player => {
+        map[player.id] = {
+          "name": player.name,
+          "team": player.team
+        };
+      });
+
+      af.database.list('/league/110296/gameweeks').subscribe(gameweeks => {
+        this._ngZone.run(() => {
+          console.log(gameweeks);
+          this.currentGameweek = gameweeks[0]["$value"];
+
+          this.results = gameweeks[1];
+
+          const sortGameweek = (a, b) => b.totalPoints - a.totalPoints;
+
+          // compute total points
+          this.results.reduce((previous, current, currentIndex) => {
+            if (previous === null) {
+              current.forEach(gameweekResult => {
+                gameweekResult["totalPoints"] = gameweekResult["points"];
+              });
+            } else {
+              current.forEach((gameweekResult, index) => {
+                gameweekResult["totalPoints"] = gameweekResult["points"] + previous[index]["totalPoints"] - gameweekResult["transferCost"];
+              });
+            }
+
+            return current;
+
+          }, null);
+
+          this.results.forEach(gameweekResults => gameweekResults.sort(sortGameweek));
+
+        })
+      });
+
+      this.playersMap = map;
+
     });
+
+
+
+
   }
 
   ngOnInit() {
